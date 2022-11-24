b59558f509f187ecb2b7a2de99a55e7f99d42828
anton13602 <an-kochetov@mail.ru>
2022-11-24 03:52:06 +0300

    refactor: change loader

--- project-management-app/src/app/components/Loader/Loader.tsx
+++ project-management-app/src/app/components/Loader/Loader.tsx
@@ -1,10 +1,10 @@
 import CircularProgress from "@mui/material/CircularProgress";
 import { Container } from "@mui/material";
 
-export function Loader() {
+export function Loader({flag} : {flag: boolean}) {
   return (
     <Container sx={{ display: "flex", justifyContent: "center", margin: "25% auto" }}>
-      <CircularProgress disableShrink={false} size={70} sx={{color: '#fff', opacity: '0.7'}} />
+      <CircularProgress disableShrink={flag} size={70} sx={{color: 'red', opacity: '0.7'}} />
     </Container>
   );
 }
